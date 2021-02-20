868f53d0ded6be3797aad8c04fde65fe72ea1eb4
Yi Ding <yi.ding.2091@gmail.com>
2021-02-20 15:23:07 -0500

    Update

---
+++ Flashcard App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flashcard App
+//
+//  Created by Yi Ding on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var answerLable: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    }
+    
+}
+
