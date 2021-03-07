d6e36f6efc50d01b48c7233c6b8f62618a37bf02
Yi Ding <yi.ding.2091@gmail.com>
2021-03-06 23:28:43 -0500

    Lab 2 Update

---
+++ Flashcard App/CreationViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CreationViewController.swift
+//  Flashcard App
+//
+//  Created by Yi Ding on 3/6/21.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+   
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    var initiaQuestion: String?
+    var initialAnswer: String?
+    @IBOutlet weak var extraAnswer1: UITextField!
+    @IBOutlet weak var extraAnswer2: UITextField!
+    var initialExtraAnswer1: String?
+    var initialExtraAnswer2: String?
+    
+    var flashcardsController: ViewController!//have the flashcardsController property you can actually call updateFlashcard() on it
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        questionTextField.text = initiaQuestion
+        answerTextField.text = initialAnswer
+        extraAnswer1.text = initialExtraAnswer1
+        extraAnswer2.text = initialExtraAnswer2
+    }
+    
+    @IBAction func didTapOnCancle(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText =  questionTextField.text
+        let answerText = answerTextField.text
+        if questionText == nil || answerText == nil || questionText!.isEmpty || answerText!.isEmpty {
+            let alert = UIAlertController(title: "Missing text", message: "You need to enter both a question and an answer", preferredStyle: .alert)
+            present(alert, animated: true)
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+        } else {
+            flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, extraAnswerOne: extraAnswer1.text!, extraAnswerTwo: extraAnswer2.text!)
+          dismiss(animated: true)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcard App/ViewController.swift
+++ Flashcard App/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var Plus: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,16 +45,41 @@ class ViewController: UIViewController {
         button3.layer.borderColor = #colorLiteral(red: 0.6816284657, green: 0.6982917786, blue: 1, alpha: 1)
     }
 
-//    @IBAction func didTapOnFlashcard(_ sender: Any) {
-//        if questionLabel.isHidden == false {
-//            questionLabel.isHidden = true
-//            answerLable.isHidden = false
-//        } else if questionLabel.isHidden == true {
-//            questionLabel.isHidden = false
-//            answerLable.isHidden = true
-//        }
-//
-//    }
+      @IBAction func didTapOnFlashcard(_ sender: Any) {
+          if questionLabel.isHidden == false {
+              questionLabel.isHidden = true
+              answerLable.isHidden = false
+          } else if questionLabel.isHidden == true {
+              questionLabel.isHidden = false
+              answerLable.isHidden = true
+          }
+      }
+    
+
+    @IBAction func plusQuestion(_ sender: Any) {
+        performSegue(withIdentifier: "addQuestion", sender: self)
+    }
+    
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+            creationController.flashcardsController = self
+        if segue.identifier == "changeQuestion" {
+            creationController.initiaQuestion = questionLabel.text
+            creationController.initialAnswer = answerLable.text
+            creationController.initialExtraAnswer1 = button1.title(for: .normal)
+            creationController.initialExtraAnswer2 = button2.title(for: .normal)
+        }
+    }
+    
+    func updateFlashcard(question: String, answer: String, extraAnswerOne: String?, extraAnswerTwo: String?){
+        questionLabel.text = question
+        answerLable.text = answer
+        
+        button1.setTitle(extraAnswerOne, for: .normal)
+        button2.setTitle(extraAnswerTwo, for: .normal)
+        button3.setTitle(answer, for: .normal)
+    }
    
     @IBAction func didTapButton1(_ sender: Any) {
         button1.isHidden = true;
@@ -68,5 +94,6 @@ class ViewController: UIViewController {
         answerLable.isHidden = false
     }
     
+
 }
 
